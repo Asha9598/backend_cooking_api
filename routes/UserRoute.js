d739e88c23d245d4468039c3afb3d773598ba85d
Asha9598 <asha.paudel9598@gmail.com>
2021-04-20 01:29:12 +0545

    Recipe router is completed

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,80 +1,126 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/userModels')
- const {check,validationResult, body}=require('express-validator');
- const bcryptjs=require('bcryptjs');
-//  const userauth=require('../middleware/userauth')
-const user = require('../models/userModels');
-const jwt =require('jsonwebtoken')
+const User = require('../models/userModel');
+const { check, validationResult } = require('express-validator');
+const bcryptjs = require('bcryptjs');
+const { verifyUser } = require('../middleware/auth');
+const Jwt = require('jsonwebtoken');
+const { userImageUpload } = require('../middleware/imageUpload');
 
-router.post('/user/insert',[
-    check('Email',"Email is required!").not().isEmpty(),
-    //check('Email',"It is not Valid Email!").isEmpty(),
-    // check('FirstName',"First Name is required!").not().isEmpty(),
-    // check('LastNmae',"Last Name is required!").not().isEmpty(),
-    // check('UserName',"User Name is required!").not().isEmpty()
+router.post('/register', [
+    check('fullName', "fullName is required!").not().isEmpty(),
+    check('userName', "userName is required!").not().isEmpty(),
+    check('email', "email is required!").not().isEmpty(),
+    check('password', "password is required!").not().isEmpty()
+], (req, res, next) => {
+    console.log(req.body);
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        const fullName = req.body.fullName;
+        const userName = req.body.userName;
+        const email = req.body.email;
+        const password = req.body.password;
 
-
-],function(req,res){
-    const errors=validationResult(req);
-    if(errors.isEmpty())
-    {
-     const FirstName = req.body.FirstName;
-    const LastName = req.body.LastName;
-    const Email = req.body.Email;
-    const UserName = req.body.UserName;
-    const Password = req.body.Password;
-    bcryptjs.hash(Password,10,function(err,hash){
-    const data2 = new User ({
-        FirstName:FirstName,
-        LastName:LastName,
-        UserName:UserName,
-        Email:Email,
-        Password:hash
-    })
-    data2.save()
-    .then(function(result){
-
-        res.status(201).json({success: true,message:"Registration of user success"});
-
-    })
-.catch(function(e){
-    res.status(500).json({message:e})
-}
-    )
-    // res.send("User Registered Sucessfully !!")
-
-    })
-}
-else{
-        res.status(400).json(errors.array())
-
-}  
-      
-})
-
-
-
-router.post('/user/login',function(req,res){
-    user.findOne({UserName:req.body.UserName})
-    .then(function(userdata){
-        if( userdata === null){
-            return res.status(200).json( {errormessage:"Authentication fail"})
-                }
-                bcryptjs.compare(req.body.Password,userdata.Password,function(err,result){
-                    if(result===false){
-                        return res.status(200).json({message:"Auth fail!"})
+        bcryptjs.hash(password, 10, (err, hash) => {
+            const data = new User({ fullName: fullName, userName: userName, email: email, password: hash });
+            if (req.body.role) {
+                data.role = req.body.role;
+            }
+            data.save()
+                .then(result => {
+                    if (result) {
+                        const token = Jwt.sign({ uid: data._id }, 'secretkey');
+                        res.status(201).json({
+                            success: true,
+                            message: "Successfully Registered!!",
+                            token,
+                            user: data
+                        });
                     }
-                    
-                   const token= jwt.sign({UserId:userdata._id},'secretkey');
-                   res.status(200).json({message:"Auth sucess!!",token:token,userType:'Customer'})
-                    
+                })
+                .catch(err => {
+                    console.log(err.message);
+                    res.status(500).json({
+                        message: "something went wrong",
+                        success: false
+                    });
 
-                })     
+                });
+        });
+    } else {
+        console.log(errors.array());
+        let simplifiedErrors = {};
+        errors.array().forEach(error => {
+            simplifiedErrors[error.param] = error.msg;
+        });
+        res.status(400).json({ success: false, message: simplifiedErrors });
+    }
+});
+
+router.post('/login', (req, res, next) => {
+    User.findOne({ userName: req.body.userName })
+        .then(user => {
+
+            if (user == null) {
+                return res.status(401).json({ message: "Authentication Failed!!" });
+            }
+
+            bcryptjs.compare(req.body.password, user.password, (err, cresult) => {
+                if (cresult === true && !err) {
+                    const token = Jwt.sign({ uid: user._id }, 'secretkey');
+                    res.status(200).json({ success: true, message: "Authentication successful", token: token, user: user });
+                } else {
+                    res.status(400).json({ success: false, message: "Incorrect password" });
+                }
+            });
 
         })
+        .catch(e => {
+            res.status(200).json({ success: false, message: "Authentication failed" });
+        });
 
-    .catch()
-})
+});
 
-module.exports = router
+router.post("/users/isUnique", (req, res, next) => {
+    User.findOne({ userName: req.body.userName })
+        .then(user => {
+            res.send({ success: true, isUnique: user ? false : true });
+        })
+        .catch(e => {
+            res.send({ success: false, message: "Cannot determine uniqueness of userName" });
+        });
+});
+
+router.get("/profile", verifyUser, (req, res, next) => {
+    const user = req.user;
+    user.populate("favourites").execPopulate()
+        .then(user => {
+            res.send({ success: true, user });
+        })
+        .catch(e => {
+            res.status(500).send({ success: true, message: "Unable to get user details" });
+        });
+});
+
+router.put("/profile/update", verifyUser, userImageUpload.single('image'), (req, res, next) => {
+    if (req.file)
+        req.body.image = req.file.path;
+    const user = req.user;
+    Object.keys(req.body).forEach(key => {
+        if (key == "password")
+            user[key] = bcryptjs.hashSync(req.body[key], 10);
+        else
+            user[key] = req.body[key];
+    });
+    user.save()
+        .then(user => {
+            res.send({ success: true, user });
+        })
+        .catch(e => {
+            res.status(500).send({ success: true, message: "Unable to update your details" });
+        });
+
+});
+
+
+module.exports = router;
--- routes/recipeRoute.js
+++ routes/recipeRoute.js
@@ -1,46 +1,193 @@
 const express = require('express');
 const router = express.Router();
-const recipe = require('../models/recipeModel')
-const {check,validationResult, body}=require('express-validator');
-const userauth=require('../middleware/userauth')
-const upload=require('../middleware/imageupload');
+const { check, validationResult } = require('express-validator');
+const { verifyAdmin, verifyUser } = require('../middleware/auth');
+const { recipeImageUpload } = require('../middleware/imageUpload');
+const Category = require('../models/categoryModel');
+const Recipe = require('../models/recipeModel');
 
-router.post('/recipe/insert',
-// [
-//     check('RecipeName',"RecipeName is required!").not().isEmpty()
-// ],
-upload.single('RecipeImage'),function(req,res){
-    const errors=validationResult(req);
-    if(errors.isEmpty())
-    {
-    
- 
-        const RecipeName = req.body.RecipeName;
-        const RecipeDescription = req.body.RecipeDescription;
-        const RecipeImage = req.file.path;
-        const User = req.body.User;
-        const data2 = new recipe ({
-            RecipeName:RecipeName,
-            RecipeDescription:RecipeDescription,
-            RecipeImage : RecipeImage,
-            User:User
-        });
-
-        data2.save()
-        .then(function(result){
-
-            res.status(201).json({message:"Recipe Added Succesfully"});
-    
+router.get("/recipes/fetch", (req, res, next) => {
+    Recipe.find()
+        .then(recipes => {
+            Promise.all(recipes.map(recipe => recipe.populate("user").execPopulate()))
+                .then(recipes => res.send({ success: true, recipes }))
+                .catch(e => res.status(500).send({ success: false, message: e.message }));
         })
-        .catch(function(e){
-            res.status(500).json({message:e})
-        }); 
-    
-    } 
-    else{
-            res.status(400).json(errors.array())
-    };         
+        .catch(e => res.status(500).send({ success: false, message: "Unable to get recipes" }));
+});
+
+router.get("/recipes/fetch/:categoryID", (req, res, next) => {
+    Category.findById(req.params.categoryID)
+        .then(category => {
+            if (!category)
+                return res.status(400).send({ success: false, message: "Category not found" });
+            category.populate({
+                path: "recipes",
+                populate: {
+                    path: "user"
+                }
+            }).execPopulate()
+                .then(category => res.send({ success: true, recipes: category.recipes }))
+                .catch(e => res.status(500).json({
+                    message: "something went wrong. Unable to get recipes.",
+                    success: false
+                }));
+        })
+        .catch(e => res.status(500).json({
+            message: "something went wrong. Unable to get recipes.",
+            success: false
+        }));
+});
+
+router.post("/recipe/insert", verifyAdmin, recipeImageUpload.single('image'), [
+    check('name', "Recipe Name is required!").not().isEmpty(),
+    check('description', "Recipe Description is required!").not().isEmpty(),
+    check('category', "Category of recipe is required!").not().isEmpty().isMongoId()
+], function (req, res) {
+    if (!req.file) {
+        return res.status(400).json({ success: false, message: "image is required" });
+    }
+    req.body.image = req.file.path;
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        Recipe.create({ ...req.body, user: req.user.id })
+            .then(recipe => {
+                recipe.populate("category").execPopulate()
+                    .then(recipe => {
+                        const category = recipe.category;
+                        category.recipes = [...category.recipes, recipe.id];
+                        category.save()
+                            .then(() => {
+                                res.status(201).send({ success: true, recipe });
+                            })
+                            .catch(e => {
+                                res.status(400).send({ success: false, message: "Unable to add recipe" });
+                            });
+                    })
+                    .catch(e => {
+                        console.log(e.message);
+                        res.status(400).send({ success: false, message: "Unable to add recipe" });
+                    });
+            })
+            .catch(e => {
+                console.log(e.message);
+                res.status(400).send({ success: false, message: "Unable to add recipe" });
+            });
+    } else {
+        console.log(errors.array());
+        let simplifiedErrors = {};
+        errors.array().forEach(error => {
+            simplifiedErrors[error.param] = error.msg;
+        });
+        res.status(400).json({ success: false, message: simplifiedErrors });
+    }
+});
+
+router.get("/recipe/fetch/:recipeID", function (req, res) {
+    Recipe.findById(req.params.recipeID)
+        .then(function (recipe) {
+            if (!recipe)
+                return res.status(400).send({ success: false, message: "No recipe found" });
+            recipe.populate("user").execPopulate()
+                .then(function (recipe) {
+                    res.send({ success: true, recipe });
+                })
+                .catch(error => {
+                    res.status(500).send({ success: false, message: "Unable to get recipe" });
+                });
+        })
+        .catch(error => {
+            res.status(500).send({ success: false, message: "Unable to get recipe" });
+        });
 });
 
 
-module.exports=router;
+router.put("/recipe/update/:recipeID", verifyAdmin, recipeImageUpload.single('image'), function (req, res) {
+    Recipe.findById(req.params.recipeID)
+        .then(function (recipe) {
+            if (!recipe)
+                return res.status(400).send({ success: false, message: "No recipe found" });
+
+            if (req.file)
+                req.body.image = req.file.path;
+
+            Object.keys(req.body).forEach(key => {
+                recipe[key] = req.body[key];
+            });
+
+            recipe.save()
+                .then(recipe => {
+                    res.send({ success: true, recipe });
+                })
+                .catch(e => {
+                    console.log(e.message);
+                    res.status(500).send({ success: false, message: "Unable to update recipe" });
+                });
+        })
+        .catch(error => {
+            console.log(error.message);
+            res.status(500).send({ success: false, message: "Unable to update recipe" });
+        });
+});
+
+router.delete("/recipe/delete/:recipeID", verifyAdmin, (req, res, next) => {
+    Recipe.findById(req.params.recipeID)
+        .then(function (recipe) {
+            recipe.populate("category").execPopulate()
+                .then(recipe => {
+                    if (!recipe)
+                        return res.status(400).send({ success: false, message: "No recipe found" });
+                    const category = recipe.category;
+                    category.recipes = category.recipes.filter(recipeID => recipeID.toString() != recipe.id.toString());
+                    category.save()
+                        .then(() => {
+                            recipe.remove()
+                                .then(recipe => res.send({ success: true, recipe }))
+                                .catch(e => res.status(500).json({ success: false, message: "Unable to delete recipe" }));
+                        })
+                        .catch(e => res.status(500).json({ success: false, message: "Unable to delete recipe" }));
+                })
+                .catch(e => res.status(500).json({ success: false, message: "Unable to delete recipe" }));
+        })
+        .catch(e => {
+            res.status(500).json({ success: false, message: "Unable to delete recipe" });
+        });
+});
+
+router.post("/recipe/mark/favourite/:recipeID", verifyUser, (req, res, next) => {
+    Recipe.findById(req.params.recipeID)
+        .then(function (recipe) {
+            if (!recipe)
+                return res.status(400).send({ success: false, message: "No recipe found" });
+            const user = req.user;
+            user.favourites = [...user.favourites, recipe.id];
+            user.save()
+                .then(() => res.send({ success: true, recipe }))
+                .catch(e => {
+                    res.status(500).json({ success: false, message: "Unable to mark recipe favourite" });
+                });
+        })
+        .catch(e => {
+            res.status(500).json({ success: false, message: "Unable to mark recipe favourite" });
+        });
+});
+
+router.delete("/recipe/delete/favourite/:recipeID", verifyUser, (req, res, next) => {
+    Recipe.findById(req.params.recipeID)
+        .then(function (recipe) {
+            if (!recipe)
+                return res.status(400).send({ success: false, message: "No recipe found" });
+            const user = req.user;
+            user.favourites = user.favourites.filter(recipeID => recipeID.toString() != recipe.id.toString());
+            user.save()
+                .then(() => res.send({ success: true, recipe }))
+                .catch(e => {
+                    res.status(500).json({ success: false, message: "Unable to delete recipe from favourites" });
+                });
+        })
+        .catch(e => {
+            res.status(500).json({ success: false, message: "Unable to delete recipe from favourites" });
+        });
+});
+
+module.exports = router;
